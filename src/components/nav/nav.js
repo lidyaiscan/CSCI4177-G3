781ae5f4e02d25949c37ce2ad3591eba1eb3e314
xinlong <xn988864@dal.ca>
2022-06-16 12:34:29 -0300

    Xinlong update nav.js again

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -8,19 +8,8 @@ export default function Navigation() {
 
     const navigateSearch = () => {
         navigate('/search');
-    }; 
+    };
     return (
-<<<<<<< HEAD
-        <nav>
-            <a href="/login">Login</a>&nbsp;&nbsp;&nbsp;
-            <a href="/register">Register</a>&nbsp;&nbsp;&nbsp;
-            <a href="">Products</a>&nbsp;&nbsp;&nbsp;
-            <a href="">Shopping Cart</a>&nbsp;&nbsp;&nbsp;
-            <a href="/contact">Contact Us</a>&nbsp;&nbsp;&nbsp;
-        </nav>
-    );
-
-=======
         <div>
             <div id="top">
                 <div id="search">
@@ -61,5 +50,4 @@ export default function Navigation() {
             </nav>
         </div>
     )
->>>>>>> xinlong
 }
