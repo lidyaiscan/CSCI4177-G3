fda7fab84f55f7d5ecd0d58f063e4589081f45bc
liambr4 <lm493365@dal.ca>
2022-06-16 12:36:24 -0300

    Product component, favourites page, add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-import Register from './components/Reg/Reg'
-import Profile from './components/Profile/Profile';
-import Login from './components/Login/Login';
-import Contact from './components/feedback/feedback';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Register from "./components/Reg/Reg";
+import Profile from "./components/Profile/Profile";
+import Login from "./components/Login/Login";
+import Contact from "./components/feedback/feedback";
+import Favourites from "./components/Favourites/Favourites";
+import { Routes, Route } from "react-router-dom";
+// Importing bootstrap to be used across the app
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "./components/nav/nav";
 
 function App() {
-
-    return (
-        <Routes>
-            <Route path="*" element={<Login />}></Route>
-            <Route path="/" element={<Register />}></Route>
-            <Route path="/register" element={<Register />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/Search" element={<Login />}></Route>
-            <Route path="/contact" element={<Contact />}></Route>
-        </Routes>
-
-    );
+	return (
+		<div>
+			<Nav />
+			<Routes>
+				<Route path="*" element={<Login />}></Route>
+				<Route path="/" element={<Register />}></Route>
+				<Route
+					path="/register"
+					element={<Register />}
+				></Route>
+				<Route
+					path="/profile"
+					element={<Profile />}
+				></Route>
+				<Route
+					path="/login"
+					element={<Login />}
+				></Route>
+				<Route
+					path="/Search"
+					element={<Login />}
+				></Route>
+				<Route
+					path="/contact"
+					element={<Contact />}
+				></Route>
+				<Route
+					path="/favourites"
+					element={<Favourites />}
+				></Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Favourites/Favourites.js
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Product from "../Products/Product";
+import { useState } from "react";
+export default function Favourites() {
+	const [products, setProducts] = useState([
+		"Bananas",
+		"Lay's Chips",
+		"Honeycrisp Apples",
+		"Toothpaste",
+		"Flour",
+		"Sugar",
+	]);
+	// Remove product from list based on index in an array.
+	// Does not persist in any way.
+	function removeFromList(index) {
+		var copy = [...products];
+		copy.splice(index, 1);
+		setProducts(copy);
+	}
+
+	return (
+		<div>
+			<div className="container w-75">
+				<h1> Favourites: {products.length}</h1>
+				{products.map((name, index) => (
+					<Product
+						key={index}
+						product={name}
+						index={index}
+						removeFromList={() =>
+							removeFromList(index)
+						}
+					/>
+				))}
+			</div>
+		</div>
+	);
+}
