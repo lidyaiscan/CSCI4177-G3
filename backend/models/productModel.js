428aeac5be968cf385eb5a82083e6fc76f1f347d
Lidya Iscan <lidyaiscaninsense@intricategroup.com>
2022-07-15 19:36:21 -0300

    add initial backend for admin

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const productSchema = {
+    pId: String,
     name: String,
+    description: String,
     measure: String,
     unit: String,
     price: String,
+    stock: String,
     rating: String,
     ratingCount: String,
     image: String
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,4 +5,8 @@ const Product = require("../models/productModel");
 router.route("/products").get(async (req, res) => {
     Product.find().then(foundProducts => res.json(foundProducts));
 })
+
+router.route("/adminProducts").get(async (req, res) => {
+    Product.find().then(foundProducts => res.json(foundProducts));
+})
 module.exports = router;
