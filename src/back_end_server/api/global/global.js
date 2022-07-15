49f227614a4b20c8e15431ce34502303a8c0084b
XinLong Liu <xn988864@dal.ca>
2022-07-16 04:32:21 +0800

    Back-end for Assignment3

--- src/back_end_server/api/global/global.js
+++ src/back_end_server/api/global/global.js
@@ -4,5 +4,6 @@
  * developed by xinlong
  */
 
-auth_db_server = "mongodb+srv://wwwlxl:www2022lxl@cluster0.nedcyqx.mongodb.net/DB_A3";
+//auth_db_server = "mongodb+srv://wwwlxl:www2022lxl@cluster0.nedcyqx.mongodb.net/DB_A3";
+auth_db_server = "mongodb+srv://group3:csci4177@cluster0.s8dqb.mongodb.net/a3-csci4177";
 module.exports = auth_db_server;
