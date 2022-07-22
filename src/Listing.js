8b90101fa0e5f65f8d08a4fdd75836997ce22eec
Liam Brown <liamb@dal.ca>
2022-07-21 23:05:07 -0300

    Fix favourites page rendering

--- src/Listing.js
+++ src/Listing.js
@@ -1,152 +1,174 @@
 /* Written by Song Pho & Liam Brown */
 import "./App.css";
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
 //Guide to implement counter with increment and decrement buttons by Tutorial Funda: https://www.tutorialfunda.com/reactjs/increment-decrement-number-using-react-hooks-counter/
 function ButtonDecrement(props) {
-    return (
-        <button className="counter" onClick={props.onClickFunc}>
-            -
-        </button>
-    );
+	return (
+		<button className="counter" onClick={props.onClickFunc}>
+			-
+		</button>
+	);
 }
 
 function ButtonIncrement(props) {
-    return (
-        <button className="counter" onClick={props.onClickFunc}>
-            +
-        </button>
-    );
+	return (
+		<button className="counter" onClick={props.onClickFunc}>
+			+
+		</button>
+	);
 }
 
 function Display(props) {
-    return <label className="counter">{props.message}</label>;
+	return <label className="counter">{props.message}</label>;
 }
 
 function Listing() {
-    const [counter, setCounter] = useState(1);
-    const incrementCounter = () => setCounter(counter + 1);
-    let decrementCounter = () => setCounter(counter - 1);
-    if (counter <= 1) {
-        decrementCounter = () => setCounter(1);
-    }
-    const product = useLocation().state;
-    const [favorites, setFavorites] = useState(
-        localStorage.getItem("favorites")
-            ? Array.from(localStorage.getItem("favorites").split(","))
-            : []
-    );
+	const [counter, setCounter] = useState(1);
+	const incrementCounter = () => setCounter(counter + 1);
+	let decrementCounter = () => setCounter(counter - 1);
+	if (counter <= 1) {
+		decrementCounter = () => setCounter(1);
+	}
+	const [favorites, setFavorites] = useState(
+		localStorage.getItem("favorites")
+			? Array.from(
+					localStorage
+						.getItem("favorites")
+						.split(",")
+						.filter((x) => x.length > 0)
+			  )
+			: []
+	);
 
-    //retrieve product data from MongoDB
-    const [products, setProducts] = React.useState([
-        {
-            pId: "",
-            name: "",
-            measure: "",
-            unit: "",
-            price: "",
-            rating: "",
-            ratingCount: "",
-            image: "",
-            brand: ""
-        },
-    ]);
+	//retrieve product data from MongoDB
+	const [products, setProducts] = React.useState([
+		{
+			pId: "",
+			name: "",
+			measure: "",
+			unit: "",
+			price: "",
+			rating: "",
+			ratingCount: "",
+			image: "",
+			brand: "",
+		},
+	]);
 
-    let productIdParam = window.location.search;
-    let query = new URLSearchParams(productIdParam);
-    let idQuery = query.get("id");
-    console.log(idQuery);
-    const fetchProducts = async () => {
-        const prods = await (
-            await fetch(`https://food4u-backend.herokuapp.com/product/${idQuery}`)
-        ).json();
-        setProducts(prods);
-    };
-    React.useEffect(() => {
-        fetchProducts();
-    }, []);
-
-    return (
-        <div>
-            <div>
-                {products.map((productSelected) => (
-                    <p>Products/Category/Sub category/{productSelected.name}</p>
-                ))}
-            </div>
-            <div id="product-detail">
-                <div className="listingDetailImg col-md-3">
-                    {products.map((productSelected) => (
-                        <img
-                            src={productSelected.image}
-                            alt={productSelected.name}
-                            className="listing-photo-large"
-                        />
-                    ))}
-                    {favorites.length > 0 &&
-                        favorites &&
-                        favorites !== undefined &&
-                        favorites.find(
-                            (pName) =>
-                                pName.toLocaleLowerCase() === products.map(productSelected => productSelected.name.toLocaleLowerCase())
-                        ) ? (
-                        <Button
-                            className="btn btn-secondary"
-                            onClick={(e) => {
-                                var copy = [...favorites];
-                                copy.splice(copy.indexOf(`${products.map(productSelected => productSelected.name)}`), 1);
-                                localStorage.setItem("favorites", copy);
-                                setFavorites(localStorage.getItem("favorites").split(","));
-                            }}
-                        >
-                            Remove from Favourites
-                        </Button>
-                    ) : (
-                        <Button
-                            className="cart"
-                            onClick={() => {
-                                if (!favorites.find((pName) => pName === products.map(productSelected => productSelected.name))) {
-                                    localStorage.setItem(
-                                        "favorites",
-                                        favorites.concat(`${products.map(productSelected => productSelected.name)}`)
-                                    );
-                                    setFavorites(localStorage.getItem("favorites").split(","));
-                                }
-                            }}
-                        >
-                            Favourite
-                        </Button>
-                    )}
-                </div>
-                <div className="listingDetail">
-                    {products.map((productSelected) => (
-                        <h2>{productSelected.name}</h2>
-                    ))}
-                    {products.map((productSelected) => (
-                        <p>
-                            {productSelected.measure} {productSelected.unit}
-                        </p>
-                    ))}
-                    {products.map((productSelected) => (
-                        <p>
-                            {productSelected.rating}⭐ ({productSelected.ratingCount})
-                        </p>
-                    ))}
-                    <h3>Quantity</h3>
-                    <ButtonDecrement onClickFunc={decrementCounter} />
-                    <Display message={counter} />
-                    <ButtonIncrement onClickFunc={incrementCounter} />
-                    <Button className="cart">Add to cart</Button>
-                </div>
-            </div>
-            <div className="listingDescription">
-                <h3>Product description</h3>
-                {products.map((productSelected) => (
-                    <p>{productSelected.description}</p>
-                ))}
-            </div>
-        </div>
-    );
+	let productIdParam = window.location.search;
+	let query = new URLSearchParams(productIdParam);
+	let idQuery = query.get("id");
+	const fetchProducts = async () => {
+		const prods = await (
+			await fetch(`https://food4u-backend.herokuapp.com/product/${idQuery}`)
+		).json();
+		setProducts(prods);
+	};
+	React.useEffect(() => {
+		fetchProducts();
+	}, []);
+	return (
+		<div>
+			<div>
+				{products.map((productSelected, key) => (
+					<p key={key}>Products/Category/Sub category/{productSelected.name}</p>
+				))}
+			</div>
+			<div id="product-detail">
+				<div className="listingDetailImg col-md-3">
+					{products.map((productSelected, key) => (
+						<div key={key}>
+							<img
+								src={productSelected.image}
+								alt={productSelected.name}
+								className="listing-photo-large"
+							/>
+							{favorites.find(
+								(pName) =>
+									pName.toLocaleLowerCase() ===
+									productSelected.name.toLocaleLowerCase()
+							) ? (
+								<Button
+									className="btn btn-secondary"
+									onClick={(e) => {
+										var copy = [...favorites];
+										copy.splice(
+											copy.indexOf(
+												`${products.map(
+													(productSelected) => productSelected.name
+												)}`
+											),
+											1
+										);
+										localStorage.setItem("favorites", copy);
+										setFavorites(localStorage.getItem("favorites").split(","));
+									}}
+								>
+									Remove from Favourites
+								</Button>
+							) : (
+								<Button
+									className="cart"
+									onClick={() => {
+										if (
+											!favorites.find(
+												(pName) =>
+													pName ===
+													products.map(
+														(productSelected) => productSelected.name
+													)
+											)
+										) {
+											localStorage.setItem(
+												"favorites",
+												favorites.concat(
+													`${products.map(
+														(productSelected) => productSelected.name
+													)}`
+												)
+											);
+											setFavorites(
+												localStorage.getItem("favorites").split(",")
+											);
+										}
+									}}
+								>
+									Favourite
+								</Button>
+							)}
+						</div>
+					))}
+				</div>
+				<div className="listingDetail">
+					{products.map((productSelected, key) => (
+						<h2 key={key}>{productSelected.name}</h2>
+					))}
+					{products.map((productSelected, key) => (
+						<p key={key}>
+							{productSelected.measure} {productSelected.unit}
+						</p>
+					))}
+					{products.map((productSelected, key) => (
+						<p key={key}>
+							{productSelected.rating}⭐ ({productSelected.ratingCount})
+						</p>
+					))}
+					<h3>Quantity</h3>
+					<ButtonDecrement onClickFunc={decrementCounter} />
+					<Display message={counter} />
+					<ButtonIncrement onClickFunc={incrementCounter} />
+					<Button className="cart">Add to cart</Button>
+				</div>
+			</div>
+			<div className="listingDescription">
+				<h3>Product description</h3>
+				{products.map((productSelected, key) => (
+					<p key={key}>{productSelected.description}</p>
+				))}
+			</div>
+		</div>
+	);
 }
 export default Listing;
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,73 +1,75 @@
 /* Written by Song Pho */
 import React, { useState } from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import "../../style.css";
-import '../../App.css';
+import "../../App.css";
 import "../../styles/navbar.css";
 import { Link, useNavigate } from "react-router-dom";
 import { Form } from "react-bootstrap";
 
 export default function Navigation() {
-    const navigate = useNavigate();
-    //search function
-    const [search, setSearch] = useState('');
-    const navigateSearch = () => {
-        navigate(`/search?${search}`);
-    };
-    return (
-        <div>
-            <div id="top">
-                <div>
-                    <Form id="search">
-                        <Form.Group controlId="searchBar">
-                            <Form.Control
-                                name="searchQuery"
-                                type="text"
-                                placeholder="Search products"
-                                fullwidth
-                            />
-                            <span><button
-                                type="Submit"
-                                value="Submit"
-                                className="Submit"
-                                onClick={navigateSearch}
-                            >
-                                Submit
-                            </button></span>
-                        </Form.Group>
-                    </Form>
-                </div>
-                <div className="top-links">
-                    <a href="/register">Sign up</a>
-                </div>
-                <div className="top-links">
-                    <a href="/login">Log in</a>
-                </div>
-                <div className="top-links">
-                    <a href="/cart">Cart</a>
-                </div>
-            </div>
-            <nav className="navigation">
-                <div className="navigation-menu">
-                    <ul>
-                        <li>
-                            <Link to="">Products</Link>
-                        </li>
-                        <li>
-                            <Link to="/flyers">Flyers</Link>
-                        </li>
-                        <li>
-                            <Link to="/delivery">Delivery & pick-up</Link>
-                        </li>
-                        <li>
-                            <Link to="/favourites">Favourites</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact Us</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    );
+	const navigate = useNavigate();
+	//search function
+	const [search, setSearch] = useState("");
+	const navigateSearch = () => {
+		navigate(`/search?${search}`);
+	};
+	return (
+		<div>
+			<div id="top">
+				<div>
+					<Form id="search">
+						<Form.Group controlId="searchBar">
+							<Form.Control
+								name="searchQuery"
+								type="text"
+								placeholder="Search products"
+								fullwidth="100%"
+							/>
+							<span>
+								<button
+									type="Submit"
+									value="Submit"
+									className="Submit"
+									onClick={navigateSearch}
+								>
+									Submit
+								</button>
+							</span>
+						</Form.Group>
+					</Form>
+				</div>
+				<div className="top-links">
+					<a href="/register">Sign up</a>
+				</div>
+				<div className="top-links">
+					<a href="/login">Log in</a>
+				</div>
+				<div className="top-links">
+					<a href="/cart">Cart</a>
+				</div>
+			</div>
+			<nav className="navigation">
+				<div className="navigation-menu">
+					<ul>
+						<li>
+							<Link to="">Products</Link>
+						</li>
+						<li>
+							<Link to="/flyers">Flyers</Link>
+						</li>
+						<li>
+							<Link to="/delivery">Delivery & pick-up</Link>
+						</li>
+						<li>
+							<Link to="/favourites">Favourites</Link>
+						</li>
+						<li>
+							<Link to="/contact">Contact Us</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
 }
